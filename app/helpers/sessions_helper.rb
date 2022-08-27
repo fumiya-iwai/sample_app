3efb3f10a8cfc7765f785a27bdb4184cb86041ae
fumiya-iwai <yyffii0912@gmail.com>
2022-08-27 14:25:11 +0000

    10.2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -25,6 +25,10 @@ module SessionsHelper
     end
   end
   
+  def current_user?(user)
+    user && user == current_user
+  end
+  
   #ユーザーがログインしていればtrue, その他ならfalseを返す
   def logged_in?
     !current_user.nil?
@@ -44,4 +48,14 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
   
+  #記憶したURL（もしくはデフォルト値)にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+  
+  #アクセスしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
